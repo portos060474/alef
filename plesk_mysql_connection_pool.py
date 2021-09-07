052681e361b086e5d18bf7f9354fcca446c1e04d
Cristian Constantin <cconstan@adobe.com>
2021-09-07 11:47:45 +0300

    present data

--- plesk_mysql_connection_pool.py
+++ plesk_mysql_connection_pool.py
@@ -1,6 +1,9 @@
 
+# !flask/bin/python
+from flask import Flask, render_template
 import mysql.connector
 
+app = Flask(__name__)
 
 
 
@@ -14,4 +17,35 @@ cnx = mysql.connector.connect(
     pool_size = 3
     )
 
+    
+
+
+
     # cnx.close()
+
+
+@app.route('/log', methods=['GET'])
+def log_data():
+    """
+    Logs data
+    """
+
+    curA = cnx.cursor(buffered=True)
+    query = ("SELECT * from tutorials_tbl")
+
+    curA.execute(query)
+
+    # Iterate through the result of curA
+    my_dict = []
+
+    for (tutorial_id, tutorial_title, tutorial_author, submission_date) in curA:
+        print(tutorial_title, tutorial_author, submission_date)
+        my_dict.append({'tutorial_title': tutorial_title,
+                            'tutorial_author': tutorial_author,
+                            'submission_date': submission_date
+                            })
+
+    return render_template('index.html', results=my_dict, xxx=1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
